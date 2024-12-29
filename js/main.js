768d8bea0dabc61ef5145ca89e615ecef09c09ce
NinB <ninobrunelle@gmail.com>
2024-12-28 20:42:18 -0800

    Version selector and new design

---
+++ js/main.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const versionCompatibility = {
+        61: "1.21.4",
+        57: "1.21.2-1.21.3",
+        48: "1.21-1.21.1",
+        41: "1.20.5-1.20.6",
+        26: "1.20.3-1.20.4",
+        18: "1.20.2",
+        15: "1.20-1.20.1",
+        12: "1.19.4",
+        10: "1.19-1.19.3",
+        9: "1.18.2",
+        8: "1.18-1.18.1",
+        7: "1.17-1.17.1",
+        6: "1.16.2-1.16.5",
+        5: "1.15-1.16.1",
+        4: "1.13-1.14.4"
+    };
+
+    const versionSelect = document.getElementById('version-select');
+    const compatibilityText = document.getElementById('compatibility-text');
+
+    function updateCompatibilityText() {
+        const defaultVersion = versionSelect.options[versionSelect.selectedIndex].value;
+        compatibilityText.innerHTML = `Datapack version <b>${defaultVersion}</b> is compatible with Minecraft version <b>${versionCompatibility[defaultVersion]}</b>`;
+
+        versionSelect.addEventListener('change', () => {
+            const selectedVersion = versionSelect.value;
+            const minecraftVersion = versionCompatibility[selectedVersion];
+            compatibilityText.innerHTML = `Datapack version <b>${selectedVersion}</b> is compatible with Minecraft version <b>${minecraftVersion}</b>`;
+        });
+    }
+
+    updateCompatibilityText();
+
+    function generateFileStructure() {
+        const fileStructure = [];
+        document.querySelectorAll('#file-structure input[type="checkbox"]').forEach(checkbox => {
+            if (checkbox.checked) {
+                fileStructure.push(checkbox.parentElement.textContent.trim());
+            }
+        });
+        return fileStructure;
+    }
+
+    function downloadZip(fileStructure) {
+        const zip = new JSZip();
+        fileStructure.forEach(path => {
+            if (path.endsWith('/')) {
+                zip.folder(path);
+            } else {
+                zip.file(path, ""); // Empty file
+            }
+        });
+        zip.generateAsync({ type: "blob" }).then(content => {
+            saveAs(content, "datapack.zip");
+        });
+    }
+
+    document.getElementById('generate-button').addEventListener('click', () => {
+        const fileStructure = generateFileStructure();
+        downloadZip(fileStructure);
+    });
+});
